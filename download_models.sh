60ed784a4f780cd2e65dda06134c1e12e4d82ad5
dkubatov1 <146163817+dkubatov1@users.noreply.github.com>
2025-08-26 21:52:45 -0400

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -11,32 +11,32 @@ export HF_ENDPOINT=https://hf-mirror.com
 
 echo "Start download models from HuggingFace mirror site"
 
-# Download Whisper weights
+# Download Whisper weights (3.4 Gb)
 hf download  TMElyralab/MuseTalk \
   --local-dir $CheckpointsDir/musetalk-1.5 \
   --include "musetalkV15/musetalk.json" "musetalkV15/unet.pth"
 
-# Download Whisper weights
+# Download Whisper weights (151 Mb)
 hf download openai/whisper-tiny \
   --local-dir $CheckpointsDir/whisper \
   --include "config.json" "pytorch_model.bin" "preprocessor_config.json"
 
-# Download SD VAE weights
+# Download SD VAE weights (335 Mb)
 hf download stabilityai/sd-vae-ft-mse \
   --local-dir $CheckpointsDir/sd-vae \
   --include "config.json" "diffusion_pytorch_model.bin"
 
-# Download SyncNet weights
+# Download SyncNet weights (5.7 Gb)
 hf download ByteDance/LatentSync-1.6 \
   --local-dir $CheckpointsDir/syncnet \
   --include "latentsync_syncnet.pt" "latentsync_unet.pt"
 
-# Download DWPose weights
+# Download DWPose weights (407 Mb)
 hf download yzd-v/DWPose \
     --local-dir $CheckpointsDir/dwpose \
     --include "dw-ll_ucoco_384.pth"
 
-# Download Face Parse Bisent and ResNet weights
+# Download Face Parse Bisent and ResNet weights (100 Mb)
 hf download ManyOtherFunctions/face-parse-bisent \
     --local-dir $CheckpointsDir/face-parse-bisent \
     --include "79999_iter.pth" "resnet18-5c106cde.pth"
