fa024c11dde6c165c466d67ffcd43ca33eb64694
dkubatov1 <146163817+dkubatov1@users.noreply.github.com>
2025-09-03 18:08:26 -0400

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -6,15 +6,7 @@ mkdir "pretrained_models"
 
 pip install -U "huggingface_hub[hf_xet]"
 
-# Mirror site to huggingface
-export HF_ENDPOINT=https://hf-mirror.com
-
 echo "Start download models from HuggingFace mirror site"
-
-# Download Whisper weights (3.4 Gb)
-hf download  TMElyralab/MuseTalk \
-  --local-dir $CheckpointsDir/musetalk-1.5 \
-  --include "musetalkV15/musetalk.json" "musetalkV15/unet.pth"
 
 # Download Whisper weights (151 Mb)
 hf download openai/whisper-tiny \
@@ -30,16 +22,6 @@ hf download stabilityai/sd-vae-ft-mse \
 hf download ByteDance/LatentSync-1.6 \
   --local-dir $CheckpointsDir/syncnet \
   --include "latentsync_syncnet.pt" "latentsync_unet.pt"
-
-# Download DWPose weights (407 Mb)
-hf download yzd-v/DWPose \
-    --local-dir $CheckpointsDir/dwpose \
-    --include "dw-ll_ucoco_384.pth"
-
-# Download Face Parse Bisent and ResNet weights (100 Mb)
-hf download ManyOtherFunctions/face-parse-bisent \
-    --local-dir $CheckpointsDir/face-parse-bisent \
-    --include "79999_iter.pth" "resnet18-5c106cde.pth"
 
 if [ $? -ne 0 ]; then
     echo "Failed to download models."
